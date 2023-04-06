71c31998f56b0254feadd21e651ab131238792a0
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:06:41 -0600

    Loop For en JS

---
+++ for.js
@@ -0,0 +1,11 @@
+// Con el bucle for podemos iterar sobre un objeto o ejecutar codigo mientras una condicion se cumpla
+/**
+ * SINTAXIS:
+ * for (i = 0; i < 10; i++){
+ *  console.log(i);
+ * }
+ */
+
+for (i = 0; i < 10; i += 2) {
+    console.log(i)
+}
