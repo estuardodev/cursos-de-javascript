eb43b6faed73249ca818f8584599036d3733a6c5
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:06:22 -0600

    Array en JS

---
+++ array.js
@@ -0,0 +1,12 @@
+// Array es una estructura de datos o tambien conocida como listas.
+var array = [1, 2, 3];
+console.log(array);
+
+console.log(array[0]); // Visualizamos el elemento en el indice 0
+
+// Metodos
+console.log(array.push(5)); // Agrega 5 al final
+console.log(array.unshift(0)); // Agrega 0 al inicio
+console.log(array.pop()); // Elimina el ultimo elemento
+console.log(array.shift()); // Elimina el primer elemento
+console.log(array.indexOf(2)); // Devuelve el indice del elemento 2
