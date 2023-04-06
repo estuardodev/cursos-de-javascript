0bfda86e4246d89b6280f9bf14b366c26db9f203
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:02:57 -0600

    Variables en JS

---
+++ variables.js
@@ -0,0 +1,41 @@
+// Variables en Js
+// En variables existen 2 tipos de estados en variables
+// Declarar: Le decimos a JS que reserve en memoria un espacio.
+// Inicializar: Le asignamos un valor al espacio en memoria que fue reservado anteriormente
+
+/* 
+var Es la forma con la cual se declaraban variables hasta ECMAScript 5. (Aún lo puedes seguir usando)
+Sintaxis:
+    var <nombre> = valor
+*/
+
+// Variable var
+var nombre = "Estuardo";
+
+// Declarar
+var edad;
+// Inicializar
+edad = 19;
+
+// Para inicializar una variable utilizamos el operador de asignación =
+
+// Para mandar a llamar una variable simplemente invocamos su nombre
+edad;
+
+/*
+let Es la forma en la cual podemos de declarar variables al igual que var
+Sintaxis:
+    let <nombre> = valor
+*/
+let secciones = ["Sección A", "Sección B", "Sección C"];
+secciones[1]; // Mandamos a llamar al elemento en el indice 1
+
+
+/*
+const Es la forma en la cual podemos de declarar constantes, osea elementos que nunca van a variar
+Sintaxis:
+    const <nombre> = valor
+*/
+const meses = 12;
+meses;
+
