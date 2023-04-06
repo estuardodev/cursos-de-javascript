7d5fd2f128d878fd8cecc4cc7e54f945b8a0c4f4
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:02:34 -0600

    Valores en JS

---
+++ valores.js
@@ -0,0 +1,27 @@
+// Valores primitivos en JavaScript
+/* 
+10 - Números (number)
+
+"Esto es un texto" - Texto (string)
+
+true - Booleanos (bool)
+false - Booleanos (bool)
+
+null - Valor indefinido (null)
+undefined - Valor indefinido (undefined)
+*/
+
+// Valores tipo objetos en JavaScript
+/*
+[1, 2, 3] - Array (object)
+{nombre: "Estuardo"} - Diccionario (object)
+*/
+
+// Para visualizar que tipo de dato es un dato, podemos utilizar la palabra reservada typeof de JavaScript
+typeof 10 // number
+typeof "Estuardo" // string
+typeof true // boolean
+typeof null // object o null
+typeof undefined // undefined
+typeof [1, 2, 3] // object
+typeof { nombre: "Estuardo" } // object
