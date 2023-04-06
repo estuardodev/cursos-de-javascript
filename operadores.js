523e692a91b27ef3adcdf0194d9805c74442f5ec
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:05:08 -0600

    Operadores en JS

---
+++ operadores.js
@@ -0,0 +1,73 @@
+// Operadore en JS
+// Para realizar operaciones en JavaScript es necesario conocer los diferentes tipos de operadores que necesitarás
+
+// Operadores Aritméticos
+// Suma
+2 + 3 // 5
+// Resta
+5 - 3 // 2
+// Multiplicación
+4 * 2 // 8
+// División
+6 / 2 // 3
+//Residuo
+21 % 5 // 1
+
+
+// Operadores de asignación
+var saludo = "Hola Mundo"
+
+
+// Operadores de asignación combinada
+a += b // a = a + b
+a -= b // a = a - b
+a *= b // a = a * b
+a /= b // a = a / b
+
+
+// Operadores de comparación
+//Igualdad
+"3" == 3 // true
+3 == 3 // true
+
+// Igualdad estricta
+"3" === 3 // false
+3 === 3 // true
+
+
+// Operadores de desigualdad
+//Desigualdad
+"3" != 3 // false
+3 != 3 // false
+
+// Desigualdad estricta
+"3" !== 3 // true //Desigualdad por valor (!=)
+3 !== 3 // false // Desigualdad por valor y tipo de dato (!==)
+
+// Operadores de mayor o menor
+// Menor que
+3 < 5 // true
+
+// Mayor que
+3 > 5 // false
+
+// Mayor o igual a que
+3 >= 3 // true
+3 >= 5 // false
+
+// Menor o igual a que
+3 <= 3 // true
+3 <= 5 // true
+
+// Operadores lógicos
+// El operador de disyunción o AND (&&) devuelve verdadero, si y solo si ambas expresiones son verdadero.
+// El operador de unión u OR (||) devuelve verdadero, si y solo si, alguna expresión es verdadero.
+// El operador de negación o NOT (!) devuelve el valor lógico contrario a la expresión.
+
+// Operador de incremento y decremento
+// El operador de incremento (++) y decremento (--) consiste en aumentar o
+// disminuir una unidad a una variable, respectivamente.
+//variable++
+//variable--
+//++variable
+//--variable
