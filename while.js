51828999e320dd15b49f0db8067d120389fbcefc
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-04-06 12:07:07 -0600

    Loop While en JS

---
+++ while.js
@@ -0,0 +1,16 @@
+// Bucle o loop while
+// El bucle se ejecuta mientras una condicion sea verdadera, de lo contrario se detendra
+/**
+ * SINTAXIS:
+ * while (condicion){
+ *  //codigo
+ *  // codigo para hacer que el while sea falso
+ * }
+ */
+
+var i = 0
+
+while (i < 10) {
+    i++
+    console.log(i);
+}
